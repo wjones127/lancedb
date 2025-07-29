f1b53caac47b2b3a1078c27ce3d3ac2ee9b0a416
Will Jones <willjones127@gmail.com>
2025-07-29 15:49:28 -0700

    feat: replace schema metadata method

--- python/python/tests/test_table.py
+++ python/python/tests/test_table.py
@@ -1804,3 +1804,29 @@ def test_stats(mem_db: DBConnection):
             },
         },
     }
+
+
+def test_replace_schema_metadata(mem_db: DBConnection):
+    """Test replacing schema metadata"""
+    data = [
+        {"vector": [3.1, 4.1], "item": "foo", "price": 10.0},
+        {"vector": [5.9, 26.5], "item": "bar", "price": 20.0},
+    ]
+    table = mem_db.create_table("test_metadata", data=data)
+
+    # Replace schema metadata
+    new_metadata = {
+        "description": "Test table for schema metadata",
+        "version": "1.0",
+        "created_by": "pytest",
+    }
+    table.replace_schema_metadata(new_metadata)
+
+    # Verify metadata was updated
+    updated_schema = table.schema
+    assert updated_schema.metadata is not None
+
+    # Check that our new metadata is present
+    for key, value in new_metadata.items():
+        assert key.encode() in updated_schema.metadata
+        assert updated_schema.metadata[key.encode()].decode() == value
